5203659f1d7421d3393cdd1cc6394511a3e87a41
eliyas41 <fentaeliyas@gmail.com>
2024-03-10 23:42:02 +0300

    execute login page functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [emailAddress, setEmailAddress] = useState('')
+  const [password, setPassword] = useState('')
+  // console.log(emailAddress)
+  // console.log(password)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    //collect data to send to database 
+    const data = {
+      email: emailAddress,
+      password: password
+    }
+
+    const apiUrl = "http://localhost:4000/login"
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    }
+    const response = fetch(apiUrl, requestOptions)
+    response.then(res => res.json())
+      .then(res => {
+        console.log(res)
+      })
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label> <br />
+        <input type="email" name='email' value={emailAddress} onChange={e => setEmailAddress(e.target.value)} /> <br />
+        <label htmlFor="password">Password:</label><br />
+        <input type="password" name='password' value={password} onChange={e => setPassword(e.target.value)} /><br />
+        <input type="submit" id='btn' value="Submit" />
+      </form>
+    </div>
   )
 }
 
